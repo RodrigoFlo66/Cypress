ecb77bd11881e80df8df2f45907048e706d5cc9f
RodrigoFlo666 <rodrigofloresv666@gmail.com>
2024-04-17 21:58:48 -0400

    Informe_TestCases

--- cypress/e2e/TestDatosReserva.cy.js
+++ cypress/e2e/TestDatosReserva.cy.js
@@ -1,46 +1,17 @@
 import 'cypress-file-upload';
 
 describe('Evaluar los datos de Reserva',  () => {
-    
-    it('Verificar que el rango de fechas de entrada(7/05/2024) y salida(15/05/2024) sea válida', async () =>{
-        await cy.visit('https://www.airbnb.com.bo/rooms/33996825?adults=2&category_tag=Tag%3A5348&enable_m3_private_room=true&photo_id=1125564886&search_mode=flex_destinations_search&source_impression_id=p3_1712800884_wUQZCxBnDdHvS5Qn&previous_page_section_name=1000&federated_search_id=d4201580-edbd-4178-872a-a3db2f77b786&guests=6&children=4', {
+     
+    it('Verificar que el rango de fechas de entrada(7/05/2024) y salida(15/05/2024) sea válida', () =>{
+        cy.visit('https://www.airbnb.com.bo/rooms/33996825?adults=2&category_tag=Tag%3A5348&enable_m3_private_room=true&photo_id=1125564886&search_mode=flex_destinations_search&source_impression_id=p3_1712800884_wUQZCxBnDdHvS5Qn&previous_page_section_name=1000&federated_search_id=d4201580-edbd-4178-872a-a3db2f77b786&guests=6&children=4&check_in=2024-05-07&check_out=2024-05-15', {
                 headers: {
                         "Accept": "application/json, text/plain, /",
                         "User-Agent": "axios/0.18.0"
                     }
                 })
-        await cy.wait(5500) 
-        await cy.visit('https://www.airbnb.com.bo/rooms/33996825?adults=2&category_tag=Tag%3A5348&enable_m3_private_room=true&photo_id=1125564886&search_mode=flex_destinations_search&source_impression_id=p3_1712800884_wUQZCxBnDdHvS5Qn&previous_page_section_name=1000&federated_search_id=d4201580-edbd-4178-872a-a3db2f77b786&guests=6&children=4&check_in=2024-05-07#availability-calendar', {
-                headers: {
-                        "Accept": "application/json, text/plain, /",
-                        "User-Agent": "axios/0.18.0"
-                    }
-                })
-        await cy.wait(5500)
-        await cy.visit('https://www.airbnb.com.bo/rooms/33996825?adults=2&category_tag=Tag%3A5348&enable_m3_private_room=true&photo_id=1125564886&search_mode=flex_destinations_search&source_impression_id=p3_1712800884_wUQZCxBnDdHvS5Qn&previous_page_section_name=1000&federated_search_id=d4201580-edbd-4178-872a-a3db2f77b786&guests=6&children=4&check_in=2024-05-07&check_out=2024-05-15', {
-                headers: {
-                        "Accept": "application/json, text/plain, /",
-                        "User-Agent": "axios/0.18.0"
-                    }
-                })
-        await cy.wait(10000) 
-    })  
+        cy.wait(10000) 
+    }) 
     it('Verificar que el rango de fechas de entrada(16/05/2024) y salida(24/05/2024) sea inválida', () =>{
-        cy.visit('https://www.airbnb.com.bo/rooms/33996825?adults=2&category_tag=Tag%3A5348&enable_m3_private_room=true&photo_id=1125564886&search_mode=flex_destinations_search&source_impression_id=p3_1712800884_wUQZCxBnDdHvS5Qn&previous_page_section_name=1000&federated_search_id=d4201580-edbd-4178-872a-a3db2f77b786&guests=6&children=4', {
-            headers: {
-                        "Accept": "application/json, text/plain, /",
-                        "User-Agent": "axios/0.18.0"
-                    }
-                })
-        
-        cy.wait(4500) 
-        cy.visit('https://www.airbnb.com.bo/rooms/33996825?adults=2&category_tag=Tag%3A5348&enable_m3_private_room=true&photo_id=1125564886&search_mode=flex_destinations_search&source_impression_id=p3_1712800884_wUQZCxBnDdHvS5Qn&previous_page_section_name=1000&federated_search_id=d4201580-edbd-4178-872a-a3db2f77b786&guests=6&children=4&check_in=2024-05-16#availability-calendar', {
-                headers: {
-                        "Accept": "application/json, text/plain, /",
-                        "User-Agent": "axios/0.18.0"
-                    }
-                })
-        cy.wait(4500)
         cy.visit('https://www.airbnb.com.bo/rooms/33996825?adults=2&category_tag=Tag%3A5348&enable_m3_private_room=true&photo_id=1125564886&search_mode=flex_destinations_search&source_impression_id=p3_1712800884_wUQZCxBnDdHvS5Qn&previous_page_section_name=1000&federated_search_id=d4201580-edbd-4178-872a-a3db2f77b786&guests=6&children=4&check_in=2024-05-16&check_out=2024-05-24', {
                 headers: {
                         "Accept": "application/json, text/plain, /",
@@ -50,20 +21,6 @@ describe('Evaluar los datos de Reserva',  () => {
         cy.wait(10000)     
     })
     it('Verificar que el rango de fechas de entrada(06/05/2024) y salida(02/05/2024) sea inválida', () =>{
-        cy.visit('https://www.airbnb.com.bo/rooms/33996825?adults=2&category_tag=Tag%3A5348&enable_m3_private_room=true&photo_id=1125564886&search_mode=flex_destinations_search&source_impression_id=p3_1712800884_wUQZCxBnDdHvS5Qn&previous_page_section_name=1000&federated_search_id=d4201580-edbd-4178-872a-a3db2f77b786&guests=6&children=4', {
-                headers: {
-                        "Accept": "application/json, text/plain, /",
-                        "User-Agent": "axios/0.18.0"
-                    }
-                })
-        cy.wait(4500) 
-        cy.visit('https://www.airbnb.com.bo/rooms/33996825?adults=2&category_tag=Tag%3A5348&enable_m3_private_room=true&photo_id=1125564886&search_mode=flex_destinations_search&source_impression_id=p3_1712800884_wUQZCxBnDdHvS5Qn&previous_page_section_name=1000&federated_search_id=d4201580-edbd-4178-872a-a3db2f77b786&guests=6&children=4&check_in=2024-05-06#availability-calendar', {
-                headers: {
-                        "Accept": "application/json, text/plain, /",
-                        "User-Agent": "axios/0.18.0"
-                    }
-                })
-        cy.wait(4500)
         cy.visit('https://www.airbnb.com.bo/rooms/33996825?adults=2&category_tag=Tag%3A5348&enable_m3_private_room=true&photo_id=1125564886&search_mode=flex_destinations_search&source_impression_id=p3_1712800884_wUQZCxBnDdHvS5Qn&previous_page_section_name=1000&federated_search_id=d4201580-edbd-4178-872a-a3db2f77b786&guests=6&children=4&check_in=2024-05-06&check_out=2024-05-02', {
                     headers: {
                         "Accept": "application/json, text/plain, /",
@@ -79,9 +36,8 @@ describe('Evaluar los datos de Reserva',  () => {
                         "User-Agent": "axios/0.18.0"
                     }
                 })
-        cy.wait(4500)  
-        cy.get('#GuestPicker-book_it-trigger').click({ force: true });
-        cy.wait(1500) 
+        cy.wait(2500)  
+        cy.get('#GuestPicker-book_it-trigger').click({ force: true }); 
         cy.get('[data-testid="GuestPicker-book_it-form-children-stepper-decrease-button"] > .i98ho2o').click({ force: true });
         cy.get('[data-testid="GuestPicker-book_it-form-children-stepper-decrease-button"] > .i98ho2o').click({ force: true });
         cy.get('[data-testid="GuestPicker-book_it-form-children-stepper-decrease-button"] > .i98ho2o').click({ force: true });
@@ -98,9 +54,8 @@ describe('Evaluar los datos de Reserva',  () => {
                         "User-Agent": "axios/0.18.0"
                     }
                 })
-        cy.wait(4500)  
-        cy.get('#GuestPicker-book_it-trigger').click({ force: true });
-        cy.wait(1500) 
+        cy.wait(2500)  
+        cy.get('#GuestPicker-book_it-trigger').click({ force: true }); 
         cy.get('[data-testid="GuestPicker-book_it-form-children-stepper-decrease-button"] > .i98ho2o').click({ force: true });
         cy.get('[data-testid="GuestPicker-book_it-form-children-stepper-decrease-button"] > .i98ho2o').click({ force: true });
         cy.get('[data-testid="GuestPicker-book_it-form-children-stepper-decrease-button"] > .i98ho2o').click({ force: true });
@@ -120,9 +75,8 @@ describe('Evaluar los datos de Reserva',  () => {
                         "User-Agent": "axios/0.18.0"
                     }
                 })
-        cy.wait(4500)  
-        cy.get('#GuestPicker-book_it-trigger').click({ force: true });
-        cy.wait(1500) 
+        cy.wait(2500)  
+        cy.get('#GuestPicker-book_it-trigger').click({ force: true }); 
         cy.get('[data-testid="GuestPicker-book_it-form-children-stepper-decrease-button"] > .i98ho2o').click({ force: true });
         cy.get('[data-testid="GuestPicker-book_it-form-children-stepper-decrease-button"] > .i98ho2o').click({ force: true });
         cy.get('[data-testid="GuestPicker-book_it-form-children-stepper-decrease-button"] > .i98ho2o').click({ force: true });
@@ -139,9 +93,8 @@ describe('Evaluar los datos de Reserva',  () => {
                         "User-Agent": "axios/0.18.0"
                     }
                 })
-        cy.wait(4500)  
-        cy.get('#GuestPicker-book_it-trigger').click({ force: true });
-        cy.wait(1500) 
+        cy.wait(2500)  
+        cy.get('#GuestPicker-book_it-trigger').click({ force: true }); 
         cy.get('[data-testid="GuestPicker-book_it-form-children-stepper-decrease-button"] > .i98ho2o').click({ force: true });
         cy.get('[data-testid="GuestPicker-book_it-form-children-stepper-decrease-button"] > .i98ho2o').click({ force: true });
         cy.get('[data-testid="GuestPicker-book_it-form-children-stepper-decrease-button"] > .i98ho2o').click({ force: true });
@@ -156,9 +109,8 @@ describe('Evaluar los datos de Reserva',  () => {
                         "User-Agent": "axios/0.18.0"
                     }
                 })
-        cy.wait(4500)  
+        cy.wait(2500)  
         cy.get('#GuestPicker-book_it-trigger').click({ force: true });
-        cy.wait(1500) 
         cy.get('[data-testid="GuestPicker-book_it-form-children-stepper-decrease-button"] > .i98ho2o').click({ force: true });
         cy.get('[data-testid="GuestPicker-book_it-form-children-stepper-decrease-button"] > .i98ho2o').click({ force: true });
         cy.get('[data-testid="GuestPicker-book_it-form-children-stepper-decrease-button"] > .i98ho2o').click({ force: true });
